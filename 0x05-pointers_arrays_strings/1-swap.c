61e0a56ac0e7b64d14f3a48d2e0595650071098b
DrSteelfist <kd.david2803@gmial.com>
2022-07-12 16:26:45 -0700

    ll

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**Write a function that swaps the values of two integers.
+ *
+ */
+
+void swap_int(int *var1, int *var2)
+{
+	int tmp = *var1;
+	*var1 = *var2;
+	*var2 = tmp;
+}
